fbde9c2aa3ccf2573ce924b6c488c9280dd1ac53
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-08 05:56:41 +0600

    Server config and env for db commit

--- server/config.js
+++ server/config.js
@@ -1,3 +1,6 @@
-const MONGODB_URL = "mongodb_URL";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export {MONGODB_URL}
+const MONGODB_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority&appName=${process.env.MONGODB_APP_NAME}`;
+
+export { MONGODB_URL };
