d8778b013c4cb14f8b15616c136aae20fba59e16
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-09 16:41:51 +0600

    Server get single product controller with db commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,7 +1,107 @@
 import { createDocument, findDocumentByMultipleAttributes } from "../database/appwrite.queries";
 
 const getProduct = async(req, res)=> {
-    console.log("Get product");
+    
+    const {name, type} = req.body;
+    console.log(name, type);
+
+    try {
+
+        if(!name || !type){
+            res.status(404).send({error: "All fields required"});
+        }
+    
+        else{
+
+            const prodcutAttributes = [
+                {
+                    name: "name",
+                    value: name
+                },
+                {
+                    name: "type",
+                    value: type
+                },
+            ];
+    
+            const exist = await findDocumentByMultipleAttributes(process.env.APPWRITE_PRODUCT_COLLECTION_ID, prodcutAttributes);
+            if(exist.total != 0){
+                const product = exist.documents[0];
+                res.status(200).send({product});
+            }
+            else{
+
+                res.status(404).send({message: "Cannot find this product"});
+
+            }
+    
+        }
+        
+    } catch (e) {
+        console.log(e);
+        res.status(400).send({error:e.message});
+    }
+
+}
+
+const getProductsByName = async(req, res)=> {
+    
+    const {name} = req.body;
+
+    try {
+
+        if(!name){
+            res.status(404).send({error: "Product name is required"});
+        }
+    
+        else{
+    
+            const exist = await findDocument(process.env.APPWRITE_PRODUCT_COLLECTION_ID, "name", name);
+            if(exist.total != 0){
+                const products = exist.documents;
+                res.status(200).send({products});
+            }
+            else{
+                res.status(404).send({message: "No such products"});
+            }
+    
+        }
+        
+    } catch (e) {
+        console.log(e);
+        res.status(400).send({error:e.message});
+    }
+
+}
+
+const getProductsByType = async(req, res)=> {
+    
+    const {type} = req.body;
+
+    try {
+
+        if(!type){
+            res.status(404).send({error: "Product type is required"});
+        }
+    
+        else{
+    
+            const exist = await findDocument(process.env.APPWRITE_PRODUCT_COLLECTION_ID, "type", type);
+            if(exist.total != 0){
+                const products = exist.documents;
+                res.status(200).send({products});
+            }
+            else{
+                res.status(404).send({message: "No such products"});
+            }
+    
+        }
+        
+    } catch (e) {
+        console.log(e);
+        res.status(400).send({error:e.message});
+    }
+
 }
 
 const setProduct = async(req, res)=> {
