ab1c8d46ce291bdcc29d09f888341ae81a0ff697
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-10 14:31:35 +0600

    Api md user register api test commit

---
+++ WorkingAPIs.md
@@ -0,0 +1,16 @@
+# Working API endpoints
+
+## User
+### http:localhost:4000/api/user/register
+method: POST
+body: {
+    "name" : "Jon snow",
+    "password: "jonsnow"
+}
+
+
+## Product
+
+
+## Bill
+
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,10 +2,6 @@ import express from "express";
 import { deleteUser, loginUser, registerUser } from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.status(200).send("User route is available");
-});
-
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
