22a41910c1c652510e16d01b3ca496b5bb25f1a0
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 01:35:05 +0600

    Client get product controller fix commit

--- client/controllers/product.controller.js
+++ client/controllers/product.controller.js
@@ -18,25 +18,20 @@ const getProducts = async() =>{
 
 const getProduct = async(name, type) =>{
 
+    let getProductResponse;
+
     if(!type){
         try {
-            const getProductResponse = await fetch(`/api/product/name/:${name}`);
-            const responseData = await getProductResponse.json();
-    
-            if (!getProductResponse.ok) {
-                throw Error(responseData.error);
-            }
-    
-            return responseData;
+            getProductResponse = await fetch(`/api/product/name/:${name}`);
         } catch (error) {
             console.error("Error:", error.message);
             throw Error(error.message);
         }
     }
 
-    if(!name){
+    else if(!name){
         try {
-            const getProductResponse = await fetch(`/api/product/type/:${type}`);
+            getProductResponse = await fetch(`/api/product/type/:${type}`);
         } catch (error) {
             console.error("Error:", error.message);
             throw Error(error.message);
@@ -46,7 +41,7 @@ const getProduct = async(name, type) =>{
     else{
         try {
 
-            const getProductResponse = await fetch(`/api/product/:${name}/:${type}`);
+            getProductResponse = await fetch(`/api/product/:${name}/:${type}`);
     
         } catch (error) {
             console.error("Error:", error.message);
