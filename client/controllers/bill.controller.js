a7c1c7cfaa3a22c4cd8ff769a3f6a08185d4754d
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 01:54:58 +0600

    Client get bills by attributes commit

--- client/controllers/bill.controller.js
+++ client/controllers/bill.controller.js
@@ -16,4 +16,70 @@ const getBills = async() =>{
 
 }
 
-export { getBills }
+const getBillByID = async(id) =>{
+
+    if (!id) {
+        throw Error("Bill id is required");
+    }
+
+    try {
+        const getBillResponse = await fetch(`/api/bill/get/id/:${id}`);
+        const responseData = await getBillResponse.json();
+
+        if (!registerResponse.ok) {
+            throw Error(responseData.error);
+        }
+
+        return responseData;
+    } catch (error) {
+        console.error("Error:", error.message);
+        throw Error(error.message);
+    }
+
+}
+
+const getBillsByClient = async(client) =>{
+
+    if (!client) {
+        throw Error("Bill client is required");
+    }
+
+    try {
+        const getBillResponse = await fetch(`/api/bill/get/client/:${client}`);
+        const responseData = await getBillResponse.json();
+
+        if (!registerResponse.ok) {
+            throw Error(responseData.error);
+        }
+
+        return responseData;
+    } catch (error) {
+        console.error("Error:", error.message);
+        throw Error(error.message);
+    }
+
+}
+
+const getBillsByEmployee = async(employee) =>{
+
+    if (!employee) {
+        throw Error("Bill employee is required");
+    }
+
+    try {
+        const getBillResponse = await fetch(`/api/bill/get/employee/:${employee}`);
+        const responseData = await getBillResponse.json();
+
+        if (!registerResponse.ok) {
+            throw Error(responseData.error);
+        }
+
+        return responseData;
+    } catch (error) {
+        console.error("Error:", error.message);
+        throw Error(error.message);
+    }
+
+}
+
+export { getBills, getBillByID, getBillsByClient, getBillsByEmployee }
