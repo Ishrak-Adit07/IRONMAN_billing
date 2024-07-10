86e502a11222bd89aefc1de599e2b52daebe3890
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-10 13:30:25 +0600

    Server appwrite bill schema display commit

--- server/models/bill.model.js
+++ server/models/bill.model.js
@@ -11,22 +11,22 @@
 //     }
 // );
 
-// bill: {
-//         employee: "668cc610cd153e2cdd17"
-//         client: "Artisan"
-//         products: [
-//             "668d186a0004b10c1c85",
-//             "668d1850002e54fa9b0a",
-//             "668cf7aad04178f62045"
-//         ]
-//         quantities: [
-//             10,
-//             15,
-//             10
-//         ]
-//         $id: "668e2974eb7bd682fd05"
-//         $tenant: "164902"
-//         $createdAt: "2024-07-10T06:25:56.965+00:00"
-//         $updatedAt: "2024-07-10T06:25:56.965+00:00"
-//         $permissions: []
-// }
+bill: {
+        employee: "668cc610cd153e2cdd17"
+        client: "Artisan"
+        products: [
+            "668d186a0004b10c1c85",
+            "668d1850002e54fa9b0a",
+            "668cf7aad04178f62045"
+        ]
+        quantities: [
+            10,
+            15,
+            10
+        ]
+        $id: "668e2974eb7bd682fd05"
+        $tenant: "164902"
+        $createdAt: "2024-07-10T06:25:56.965+00:00"
+        $updatedAt: "2024-07-10T06:25:56.965+00:00"
+        $permissions: []
+}
